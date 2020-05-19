4421ec55a76c29d9ddec21e609776f01412daea8
edermlemos <edermlemos@gmail.com>
2020-05-18 21:55:08 -0300

    Aulas de JavaScript

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/*
+var nome = "Eder";
+var idade = 41;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo!"
+//alert(nome + " tem " + idade + "anos.");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Japão","Brasil"));
+console.log(frase.toUpperCase());
+var fruta = {nome:"Pera", cor:"Amarela"}
+var frutas = [{nome:"Pera", cor:"Amarela"},{nome:"Uva", cor:"Vinho"}];
+console.log(frutas[0]);
+console.log(fruta.cor);
+var lista = ["Maça","Pera","Laranja"];
+console.log(lista.toString());
+console.log(lista[1]);
+
+//var idade =18;
+var idade = prompt("Qual a sua idade");
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+
+var contar = 0;
+
+while (contar <= 5){
+    console.log(contar);
+    contar = contar + 1;
+};
+
+
+
+var contar;
+for(contar=0; contar<5;contar++){
+    console.log(contar);
+};
+*/
+
+var d = new Date();
+alert(d.getMonth());
+
+
+
+
+
